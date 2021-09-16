0f690b3ef5c402a87c9e2ed52fd8be4b50b3ec42
Rahul Bansal <rbbansal558@gmail.com>
2021-09-16 23:21:12 +0530

    Removed validaor

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,22 +1,22 @@
-const { validationResult } = require('express-validator');
+// const { validationResult } = require('express-validator');
 const web3 = require('web3');
 
-const checkErrors = (req, cb) => {
-  const validateBody = validationResult.withDefaults({
-    formatter: (err) => {
-      return {
-        err: true,
-        message: err.msg,
-      };
-    },
-  });
-  const errors = validateBody(req);
-  if (!errors.isEmpty()) {
-    const { err, message } = errors.array({ onlyFirstError: true })[0];
-    return cb(err, message);
-  }
-  return cb(false);
-};
+// const checkErrors = (req, cb) => {
+//   const validateBody = validationResult.withDefaults({
+//     formatter: (err) => {
+//       return {
+//         err: true,
+//         message: err.msg,
+//       };
+//     },
+//   });
+//   const errors = validateBody(req);
+//   if (!errors.isEmpty()) {
+//     const { err, message } = errors.array({ onlyFirstError: true })[0];
+//     return cb(err, message);
+//   }
+//   return cb(false);
+// };
 
 const validWalletAddress = (address) => {
   return web3.default.utils.isAddress(address);
